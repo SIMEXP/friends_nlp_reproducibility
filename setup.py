6a2f73b2ccd75ddaba6bc8f49908b5b293171fb2
Isil Bilgin <bilginlab@gmail.com>
2022-07-20 15:42:03 -0400

    [DATALAD] Recorded changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Reproducibility study on Friends data set by utilizing the previous method developed by Huth et al. (2016)',
+    author='Isil Poyraz Bilgin',
+    license='MIT',
+)
